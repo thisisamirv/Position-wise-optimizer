5e042459769367d3070bd60521b714c5c69f46c9
thisisamirv <thisisamirv@gmail.com>
2021-07-30 17:35:28 +0430

    commit

--- Algorithm.py
+++ Algorithm.py
@@ -53,60 +53,78 @@ b3 = np.zeros((layer_dims[3], 1))
 W4 = np.random.randn(layer_dims[4], layer_dims[3]) / np.sqrt(layer_dims[3])
 b4 = np.zeros((layer_dims[4], 1))
 
-# Network
+W1n = np.copy(W1)
+W2n = np.compy(W2)
+W3n = np.copy(W3)
+W4n = np.copy(W4)
+b1n = np.copy(b1)
+b2n = np.copy(b2)
+b3n = np.copy(b3)
+b4n = np.copy(b4)
+
+W1j = np.copy(W1)
+W2j = np.copy(W2)
+W3j = np.copy(W3)
+W4j = np.copy(W4)
+b1j = np.copy(b1)
+b2j = np.copy(b2)
+b3j = np.copy(b3)
+b4j = np.copy(b4)
+
+# Normal Network
 for i in range(0, 2000):
-    Z1 = np.dot(W1, X) + b1
-    A1 = np.maximum(0, Z1)
-    Z2 = np.dot(W2, A1) + b2
-    A2 = np.maximum(0, Z2)
-    Z3 = np.dot(W3, A2) + b3
-    A3 = np.maximum(0, Z3)
-    Z4 = np.dot(W4, A3) + b4
-    A4 = 1 / (1 + np.exp(-Z4))
+    Z1n = np.dot(W1n, X) + b1n
+    A1n = np.maximum(0, Z1n)
+    Z2n = np.dot(W2n, A1n) + b2n
+    A2n = np.maximum(0, Z2n)
+    Z3n = np.dot(W3n, A2n) + b3n
+    A3n = np.maximum(0, Z3n)
+    Z4n = np.dot(W4n, A3n) + b4n
+    A4n = 1 / (1 + np.exp(-Z4n))
 
-    dZ4 = A4 - Y
-    dW4 = np.dot(dZ4, A3.T) * (1. / A3.shape[1])
-    db4 = np.sum(dZ4, axis=1, keepdims=True) * (1. / A3.shape[1])
+    dZ4n = A4n - Y
+    dW4n = np.dot(dZ4n, A3n.T) * (1. / A3n.shape[1])
+    db4n = np.sum(dZ4n, axis=1, keepdims=True) * (1. / A3n.shape[1])
 
-    dA3 = np.dot(W4.T, dZ4)
-    dZ3 = np.array(dA3, copy=True)
-    dZ3[Z3 <= 0] = 0
-    dW3 = np.dot(dZ3, A2.T) * (1. / A2.shape[1])
-    db3 = np.sum(dZ3, axis=1, keepdims=True) * (1. / A2.shape[1])
+    dA3n = np.dot(W4n.T, dZ4n)
+    dZ3n = np.array(dA3n, copy=True)
+    dZ3n[Z3n <= 0] = 0
+    dW3n = np.dot(dZ3n, A2n.T) * (1. / A2n.shape[1])
+    db3n = np.sum(dZ3n, axis=1, keepdims=True) * (1. / A2n.shape[1])
 
-    dA2 = np.dot(W3.T, dZ3)
-    dZ2 = np.array(dA2, copy=True)
-    dZ2[Z2 <= 0] = 0
-    dW2 = np.dot(dZ2, A1.T) * (1. / A1.shape[1])
-    db2 = np.sum(dZ2, axis=1, keepdims=True) * (1. / A1.shape[1])
+    dA2n = np.dot(W3n.T, dZ3n)
+    dZ2n = np.array(dA2n, copy=True)
+    dZ2n[Z2n <= 0] = 0
+    dW2n = np.dot(dZ2n, A1n.T) * (1. / A1n.shape[1])
+    db2n = np.sum(dZ2n, axis=1, keepdims=True) * (1. / A1n.shape[1])
 
-    dA1 = np.dot(W2.T, dZ2)
-    dZ1 = np.array(dA1, copy=True)
-    dZ1[Z1 <= 0] = 0
-    dW1 = np.dot(dZ1, X.T) * (1. / X.shape[1])
-    db1 = np.sum(dZ1, axis=1, keepdims=True) * (1. / X.shape[1])
+    dA1n = np.dot(W2n.T, dZ2n)
+    dZ1n = np.array(dA1n, copy=True)
+    dZ1n[Z1n <= 0] = 0
+    dW1n = np.dot(dZ1n, X.T) * (1. / X.shape[1])
+    db1n = np.sum(dZ1n, axis=1, keepdims=True) * (1. / X.shape[1])
 
-    W1 = W1 - learning_rate * dW1
-    b1 = b1 - learning_rate * db1
-    W2 = W2 - learning_rate * dW2
-    b2 = b2 - learning_rate * db2
-    W3 = W3 - learning_rate * dW3
-    b3 = b3 - learning_rate * db3
-    W4 = W4 - learning_rate * dW4
-    b4 = b4 - learning_rate * db4
+    W1n = W1n - learning_rate * dW1n
+    b1n = b1n - learning_rate * db1n
+    W2n = W2n - learning_rate * dW2n
+    b2n = b2n - learning_rate * db2n
+    W3n = W3n - learning_rate * dW3n
+    b3n = b3n - learning_rate * db3n
+    W4n = W4n - learning_rate * dW4n
+    b4n = b4n - learning_rate * db4n
     if i % 100 == 0 or i == 2000 - 1:
-        cost = (-1 / m_train_y) * np.sum(np.multiply(Y, np.log(A4)) + np.multiply(1 - Y, np.log(1 - A4)))
+        cost = (-1 / m_train_y) * np.sum(np.multiply(Y, np.log(A4n)) + np.multiply(1 - Y, np.log(1 - A4n)))
         cost = np.squeeze(cost)
         print("Cost after iteration {}: {}".format(i, cost))
 
 # Test
-Z1_test = np.dot(W1, test_x) + b1
+Z1_test = np.dot(W1n, test_x) + b1n
 A1_test = np.maximum(0, Z1_test)
-Z2_test = np.dot(W2, A1_test) + b2
+Z2_test = np.dot(W2n, A1_test) + b2n
 A2_test = np.maximum(0, Z2_test)
-Z3_test = np.dot(W3, A2_test) + b3
+Z3_test = np.dot(W3n, A2_test) + b3n
 A3_test = np.maximum(0, Z3_test)
-Z4_test = np.dot(W4, A3_test) + b4
+Z4_test = np.dot(W4n, A3_test) + b4n
 A4_test = 1 / (1 + np.exp(-Z4_test))
 
 cost_test = (-1 / m_test_y) * np.sum(
